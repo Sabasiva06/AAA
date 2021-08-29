d7fcb94f7e868883885e9fa8017d1516d1c9ea79
Sarath13-dev <ramsssjrs@gmail.com>
2021-08-29 19:22:48 +0530

    master

---
+++ src/org/samplecode/CourseDetails.java
@@ -0,0 +1,22 @@
+package org.samplecode;
+
+public class CourseDetails {
+
+	private void javaProgramm() {
+
+		System.out.println("Java Programm");
+	}
+
+	private void pythonProgramm() {
+		System.out.println("Python Programm");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		CourseDetails c =new CourseDetails();
+		c.javaProgramm();
+		c.pythonProgramm();
+	}
+
+}
